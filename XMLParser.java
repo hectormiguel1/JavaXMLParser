ec951c14979c7e4fd0e90bbe3bd066057a683247
Hector R <github.hector1@gmail.com>
2021-12-03 00:32:36 -0500

    Further work on parser

--- XMLParser.java
+++ XMLParser.java
@@ -44,57 +44,6 @@ public class XMLParser {
         }
     }
 
-    // private void parse_with_stack(String xml, Stack<String> stack) {
-    // var xmlBytes = xml.getBytes();
-    // var currentNode = root;
-    // for (int i = 0; i < xmlBytes.length;) {
-    // String tag = "";
-    // HashMap<String, String> attributes = new HashMap<>();
-    // String value = "";
-    // if (xmlBytes[i] == '<') {
-    // StringBuffer sb = new StringBuffer();
-    // while( ((char)xmlBytes[i]) != ((char)'>') || ((char)xmlBytes[i]) != ((char)'
-    // ') ) {
-    // sb.append(((char) xmlBytes[i]));
-    // i++;
-    // }
-    // tag = sb.toString();
-    // if (xmlBytes[i] == '>') {
-    // if(currentNode == null) {
-    // root = new XMLNode(tag, value, null);
-    // currentNode = root;
-    // } else {
-    // var childNode = new XMLNode(tag, value, currentNode);
-    // currentNode.addChild(childNode);
-    // currentNode = childNode;
-    // }
-    // stack.push(tag);
-    // } else if (xmlBytes[i] == ' ') {
-    // StringBuffer sb2 = new StringBuffer();
-    // while(xmlBytes[i] != '>') {
-    // sb2.append(xmlBytes[i]);
-    // i++;
-    // }
-    // var attribStr = sb2.toString();
-    // var attribs = attribStr.split(" ");
-    // attributes = new HashMap<String, String>();
-    // for (var attrib : attribs) {
-    // attributes.put(attrib.split("=")[0], attrib.split("=")[1]);
-    // }
-
-    // }
-    // } else {
-    // StringBuffer sb = new StringBuffer();
-    // while(xmlBytes[i] != '<') {
-    // sb.append(xmlBytes[i]);
-    // i++;
-    // }
-    // value = sb.toString();
-    // currentNode.setValue(value);
-    // }
-    // }
-    // }
-
     private void parse_with_stack(String xml) {
         xml = xml.replaceAll("\n", "");
         xml = xml.replaceAll("\r", "");
